9ebfc843745ceccd9e0be52d0ac09143af895b80
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-15 23:26:17 +0800

    Create var-name.js

---
+++ chapter03/var-name.js
@@ -0,0 +1,14 @@
+<script>
+        var app = 10;
+        var App = 100;
+        console.log(app);
+        console.log(App);
+
+        // var 18age;
+
+        // var var; //因为var有特殊意义了，这个叫做关键字，不能作为变量名的  for while if等
+
+        // name   //我们尽量不要直接使用name作为变量名
+
+        // console.log(tel);
+</script>
