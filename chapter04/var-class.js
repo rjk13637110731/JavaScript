f6b804be0d988412c52e9a970834c2f596f0b2c2
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-15 23:27:30 +0800

    Create var-class.js

---
+++ chapter04/var-class.js
@@ -0,0 +1,13 @@
+<script>
+        //int num = 10;   java语言
+        // var num;  // 这里的num，我们是不确定属于哪种数据类型
+        var num = 10; //num属于数字型
+        //js的变量数据类型是只有程序在运行过程中，根据等号右边的值来确定的
+
+        var str = 'pink佩奇';  //str字符串型
+
+        // js是动态语言，变量的数据类型是可以变化的
+        var x = 10;      //x是数字型
+        x = 'bill';      //x是字符串型
+        console.log(x);  //bill
+</script>
