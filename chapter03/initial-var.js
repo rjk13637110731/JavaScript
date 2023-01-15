0e3a366f2f2b800fb775860737256d8bc30b1ab0
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-15 23:23:41 +0800

    Create initial-var.js

---
+++ chapter03/initial-var.js
@@ -0,0 +1,14 @@
+<script>
+        //1.声明了一个age的变量
+        var age;
+
+        // 2.赋值 把值存入这个变量中
+        age = 18;
+
+        // 3.输出结果
+        console.log(age);   //18
+
+        // 4.变量的初始化
+        var myName = '小猪佩奇';
+        console.log(myName);
+    </script>
