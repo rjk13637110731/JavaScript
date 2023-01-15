b2612721e87447656c7bc4bcbd192fd27cfbfffe
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-15 23:24:06 +0800

    Create case-var.js

---
+++ chapter03/case-var.js
@@ -0,0 +1,9 @@
+<script>
+        var myName = prompt('请输入姓名：');
+        var age = prompt('请输入年龄：');
+        var email = prompt('请输入邮箱地址：');
+        var address = prompt('请输入家庭住址：');
+        var salary = prompt('请输入工资：');
+
+        console.log('我叫' + myName + '，我住在' + address + '，我今年' + age + '岁了，我的邮箱是' + email + '，我的工资为' + salary);
+    </script>
