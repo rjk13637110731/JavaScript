4ac74fe686eea2782a318ac19beeeaf044db5863
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:50:10 +0800

    Create zhuanyi.js

---
+++ chapter04/zhuanyi.js
@@ -0,0 +1,14 @@
+<script>
+        //'pink'  "pink" '12' 'true'
+        // var str = '我是一个'小猪佩奇'猪猪';
+        var str = '我是一个"小猪佩奇"猪猪';
+        console.log(str);
+        var str1 = "我是一个'小猪佩奇'猪猪";
+        console.log(str1);
+
+        // 字符串转义字符 都是用反斜杠 \ 开头 但是这些转义字符都要写到引号里面
+        var str2 = '我是一个\'小猪佩奇\'猪猪';
+        console.log(str2);
+        var str3 = '我是一个"小猪佩奇"\n猪猪';
+        console.log(str3);
+</script>
