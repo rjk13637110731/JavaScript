b479c0a85bf76339556dff3316f6f3b16ebdefc4
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:56:21 +0800

    Create case-3.js

---
+++ chapter04/case-3.js
@@ -0,0 +1,6 @@
+<script>
+        var myname = prompt('请输入您的姓名：');
+        var age = prompt('请输入您的年龄：');
+        var sex = prompt('请输入您的性别：');
+        alert('您的姓名是：' + myname + '\n您的年龄是：' + age + '\n您的性别是：' + sex);
+</script>
