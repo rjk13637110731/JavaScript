ecdc30578cd7d26b88ec85c5f3f3b95d68478319
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:51:22 +0800

    Create string-pack.js

---
+++ chapter04/string-pack.js
@@ -0,0 +1,12 @@
+<script>
+        // 1.检测获取字符串的长度 length
+        var str = 'my name is andy';
+        console.log(str.length);  //15
+
+        //2.字符串的拼接 +  只要有字符串和其他类型相拼接，最终的结果是字符串类型
+        console.log('沙漠' + '骆驼');   //字符串的  沙漠骆驼
+        console.log('小猪佩奇' + 18);   //'小猪佩奇18'
+        console.log('小猪佩奇' + true); //'小猪佩奇true'
+        console.log(12 + 12);          //24
+        console.log('12' + 12);        //'1212'
+</script>
