53e0a7b9cb70590721cf36fcf8517dfa91112f8b
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:52:33 +0800

    Create Boolean.js

---
+++ chapter04/Boolean.js
@@ -0,0 +1,6 @@
+<script>
+        var flag = true;  //flag布尔型
+        var flag1 = false;  //flag1 布尔型
+        console.log(flag + 1);  //true 参与加法运算，当1来看
+        console.log(flag1 + 1);  //false 参与加法运算，当0来看
+</script>
