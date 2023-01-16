cd84b5d5546ea3105007cbfa0e5ab4ef0c0a1e07
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:48:06 +0800

    Create 16-8-10.js

---
+++ chapter04/16-8-10.js
@@ -0,0 +1,18 @@
+<script>
+        var num = 10;   //num 数字型
+        var PI = 3.14;  //PI 数字型
+
+        // 1.八进制  0~7 我们程序里面数字前面加0 表示八进制
+        var num1 = 010;
+        console.log(num1);  //010 八进制转换为 十进制 就是 8
+
+        var num2 = 012;
+        console.log(num2);   //012 八进制转换为十进制就是 10
+
+        // 2.十六进制 0~9 a~f   #ffffff 数字前面加0x 表示十六进制
+        var num3 = 0x9;
+        console.log(num3);
+
+        var num4 = 0xa;
+        console.log(num4);
+</script>
