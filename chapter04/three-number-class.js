38296b3b772b0c73304c51b7f995f761b1de2aa5
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:49:16 +0800

    Create three-number-class.js

---
+++ chapter04/three-number-class.js
@@ -0,0 +1,8 @@
+<script>
+        // 5.无穷大
+        console.log(Number.MAX_VALUE * 2);  //Infinity 无穷大
+        // 6.无穷小
+        console.log(-Number.MAX_VALUE * 2);  //-Infinity 无穷小
+        // 7.非数字 NaN
+        console.log('小猪佩奇' - 100);  //NaN
+</script>
