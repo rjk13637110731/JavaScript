61d3df40e3d185be28bfa436621492b834eed5ea
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:49:42 +0800

    Create isNaN.js

---
+++ chapter04/isNaN.js
@@ -0,0 +1,5 @@
+<script>
+        //isNaN()这个方法用来判断非数字 并且返回一个值，如果是数字，返回的是false，如果不是数字，返回true
+        console.log(isNaN(12));          //false
+        console.log(isNaN('小猪佩奇'));   //true
+</script>
