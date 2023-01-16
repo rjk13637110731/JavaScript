e49b9a5335b1edb3660848bdaa3bf48d6ef5e77d
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:55:26 +0800

    Create case-2.js

---
+++ chapter04/case-2.js
@@ -0,0 +1,6 @@
+<script>
+        var num1 = prompt('请输入第一个值：');
+        var num2 = prompt('请输入第二个值：');
+        var sum = parseFloat(num1) + parseFloat(num2);
+        alert(num1 + '+' + num2 + '=' + sum);
+</script>
