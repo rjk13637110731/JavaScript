4dfed241b9738b13c019ffcff6c1b43311f3c796
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-16 13:53:22 +0800

    Create typeof.js

---
+++ chapter04/typeof.js
@@ -0,0 +1,21 @@
+<script>
+        var num = 10;
+        console.log(typeof num); //number
+
+        var str = 'pink';
+        console.log(typeof str); //string
+
+        var flag = true;
+        console.log(typeof flag); //boolean
+
+        var vari = undefined;
+        console.log(typeof vari); //undefined
+
+        var timer = null;
+        console.log(typeof timer); //object 对象数据类型
+
+        // prompt 取过来的值是字符型的
+        var age = prompt('请输入您的年龄：');
+        console.log(age);
+        console.log(typeof age); //string
+</script>
