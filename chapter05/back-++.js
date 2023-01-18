01386f7464385af58ec7ce21366189a110e8a07c
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-18 08:33:34 +0800

    Create back-++.js

---
+++ chapter05/back-++.js
@@ -0,0 +1,10 @@
+<script>
+        var num = 10;
+        num++;  //num = num + 1
+        console.log(num);
+        // 1.前置自增和后置自增如果单独使用，效果是一样的
+        // 2.后置自增 口诀：先返回原值，后自加1
+        var age = 10;
+        console.log(age++ + 10); //20
+        console.log(age);//11
+</script>
