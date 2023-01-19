bfc3262053d587e300e3ccd8adbb01253ee5274b
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-19 13:18:33 +0800

    Create -=-+=.js

---
+++ chapter05/-=-+=.js
@@ -0,0 +1,13 @@
+<script>
+        var num = 10;
+        // num = num + 1;  num++
+        //num = num + 2; // num += 2;
+        num += 2;
+        console.log(num);//12
+        num += 5;
+        console.log(num);//17
+
+        var age = 2;
+        age *= 3;
+        console.log(age);
+</script>
