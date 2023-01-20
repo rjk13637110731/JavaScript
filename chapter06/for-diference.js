6427c8fb3f44b4b028e330fd7a427fe89eb41fa5
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-20 23:51:07 +0800

    Create for-diference.js

---
+++ chapter06/for-diference.js
@@ -0,0 +1,10 @@
+//for循环可以执行相同的代码
+for (var i = 1; i <= 10; i++) {
+    console.log('小猪佩奇');
+}
+
+//我们可以让用户控制输出的次数
+var num = prompt('请您输入次数：');
+for (var i = 1; i <= num; i++) {
+    console.log('小猪乔治');
+}
