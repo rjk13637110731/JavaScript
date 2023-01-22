f9433394e9c31a760141b886d0de6d3d988b374d
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-22 23:18:45 +0800

    Create array-all.js

---
+++ chapter07/array-all.js
@@ -0,0 +1,11 @@
+// 数组索引访问数组中的元素
+var arr = ['red','green', 'blue'];
+console.log(arr[0]) // red
+console.log(arr[1]) // green
+console.log(arr[2]) // blue
+
+// for循环遍历数组
+var arr = ['red','green', 'blue'];
+for (var i = 0; i < arr.length; i++){
+    console.log(arrStus[i]);
+}
