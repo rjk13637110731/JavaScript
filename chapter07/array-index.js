61209959821003aa5ad1cc0b83748922da4669fe
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-22 23:18:27 +0800

    Create array-index.js

---
+++ chapter07/array-index.js
@@ -0,0 +1,9 @@
+// 6.获取数组元素，格式：数组名[索引号]
+var arr1 = [1, 2, '小猪佩奇', true];
+console.log(arr1[2]);//小猪佩奇
+console.log(arr1[3]);//true
+var arr2 = ['小猪佩奇', '小猪乔治', '小猪布朗'];
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[2]);
+console.log(arr2[3]);//因为没有这个数组元素，所以输出的结果是 undefined
