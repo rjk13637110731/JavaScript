c7637f5cb229140d99e1951d54d17605ece90402
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-23 23:50:41 +0800

    Create array-case1.js

---
+++ chapter07/array-case1.js
@@ -0,0 +1,13 @@
+//老师代码
+var arr = [2, 0, 6, 1, 77, 0, 52, 0, 25, 7];
+var newArr = [];   // 空数组的默认的长度为 0 
+// 定义一个变量 i 用来计算新数组的索引号
+for (var i = 0; i < arr.length; i++) {
+    // 找出大于 10 的数
+    if (arr[i] != 0) {
+        // 给新数组
+        // 每次存入一个值，newArr长度都会 +1  
+        newArr[newArr.length] = arr[i];
+    }
+}
+console.log(newArr);
