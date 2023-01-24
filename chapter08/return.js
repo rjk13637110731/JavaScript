bb5ff65c8e9e5bc561f495bfb5f627f26cfe2d41
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-24 22:55:17 +0800

    Create return.js

---
+++ chapter08/return.js
@@ -0,0 +1,15 @@
+function getResult() {
+    return 666;
+}
+getResult();//getResult() = 666;
+console.log(getResult());
+
+function cook(aru) {
+    return aru;
+}
+console.log(cook('大肘子'));
+
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+console.log(getSum(1, 2));
