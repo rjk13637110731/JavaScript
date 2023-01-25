7e7fb1d0ea6e34947411bd38dedbf2fd706379f5
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-25 22:42:50 +0800

    Create style-3.js

---
+++ chapter11/style-3.js
@@ -0,0 +1,13 @@
+function Person(name, age, sex) {
+     this.name = name;
+     this.age = age;
+     this.sex = sex;
+     this.sayHi = function() {
+      alert('我的名字叫：' + this.name + '，年龄：' + this.age + '，性别：' + this.sex);
+    }
+}
+
+var bigbai = new Person('大白', 100, '男');
+var smallbai = new Person('小白', 21, '男');
+console.log(bigbai.name);
+console.log(smallbai.name);
