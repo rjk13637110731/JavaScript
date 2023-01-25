4bf01e1c4f30c5caf5a55f906ac76900ae6aa9e0
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-25 22:41:38 +0800

    Create style-1-case.js

---
+++ chapter11/style-1-case.js
@@ -0,0 +1,12 @@
+var kekeDog = {
+    uname: keke,
+    type: alasijia,
+    age: 5,
+    color: brone,
+    bark: function () {
+        console.log('汪汪汪~');
+    },
+    showFilm: function () {
+        console.log('演电影！');
+    }
+};
