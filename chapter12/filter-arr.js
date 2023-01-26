59885bd7a911040d357cde9310b2ae8e6237fa26
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-26 22:01:39 +0800

    Create filter-arr.js

---
+++ chapter12/filter-arr.js
@@ -0,0 +1,9 @@
+var arr = [1500, 1200, 2000, 2100, 1800];
+var newArr = [];
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < 2000) {
+        // newArr[newArr.length] = arr[i];
+        newArr.push(arr[i]);
+    }
+}
+console.log(newArr);
