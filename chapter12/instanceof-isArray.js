c392b4089482b3bc4cbb618a40b8316b1954645b
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-26 20:03:02 +0800

    Create instanceof-isArray.js

---
+++ chapter12/instanceof-isArray.js
@@ -0,0 +1,6 @@
+var arr = [1, 23];
+var obj = {};
+console.log(arr instanceof Array); // true
+console.log(obj instanceof Array); // false
+console.log(Array.isArray(arr));   // true
+console.log(Array.isArray(obj));   // false
