350eb6019945d1455455cba531016196ef1982d2
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-27 20:47:52 +0800

    Create concat.js

---
+++ chapter12/concat.js
@@ -0,0 +1,10 @@
+// 字符串操作方法 字符串不会改变
+// concat('字符串1','字符串2',...);
+var str = 'andy';
+console.log(str.concat('red'));
+console.log(str);
+
+// 2.substr(截取的起始位置,截取几个字符);
+var str1 = '改革春风吹满地';
+console.log(str1.substr(2, 2));
+console.log(str1);
