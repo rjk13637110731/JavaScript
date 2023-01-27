a550c0997e09b6c312108c59781cc81037a5e2a0
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-01-27 20:43:54 +0800

    Create Array-String.js

---
+++ chapter12/Array-String.js
@@ -0,0 +1,10 @@
+// 数组转换为字符串
+// 1.toString()将我们的数组转换为字符串
+var arr = [1, 2, 3];
+console.log(arr.toString()); // 1,2,3
+
+// 2.join(分隔符)
+var arr1 = ['green', 'blue', 'pink'];
+console.log(arr1.join()); // green,blue,pink
+console.log(arr1.join('-')); // green-blue-pink
+console.log(arr1.join('&')); // green&blue&pink
